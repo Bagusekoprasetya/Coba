667e9b0f56526383ebd7e60f8d963c0798cbce21
Bagus eko <baguseko8814@gmail.com>
2019-03-31 14:16:26 +0700

    Latihan

---
+++ Latihan/LatihanMemberClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "paijo";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.taxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Latihan/LatihanMemberClass/Taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void taxiInfo()
+        {
+            Console.WriteLine("DriverName: {0}", DriverName);
+            if (OnDuty)
+                Console.WriteLine("OnDuty : Yes");
+            else
+                Console.WriteLine("OnDuty : No");
+
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+            Console.WriteLine();
+        }
+    
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+        }
+
+        public void DropOffPassenger ()
+        {
+            Console.WriteLine("{0} Selesai Menjemput penumpang", DriverName);
+               }
+
+    }
+
+}
